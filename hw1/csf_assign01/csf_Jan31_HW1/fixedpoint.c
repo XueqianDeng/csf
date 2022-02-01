4ca66142b79992512e819c6e687ddb171df2d3e9
chujianyu <yu_chn@outlook.com>
2022-01-31 23:57:07 -0500

    addded comments

--- hw1/csf_assign01/csf_Jan31_HW1/fixedpoint.c
+++ hw1/csf_assign01/csf_Jan31_HW1/fixedpoint.c
@@ -10,31 +10,38 @@
 static Fixedpoint DUMMY;
 
 Fixedpoint fixedpoint_create(uint64_t whole) {
-  // TODO: implement
-  Fixedpoint fp = {whole,0, 1};
+  Fixedpoint fp = {whole,0, 1}; //create a Fixedpoint struct, with tag defaulted to 1 (valid and non-negative)
   return fp;
 }
 
 Fixedpoint fixedpoint_create2(uint64_t whole, uint64_t frac) {
-  // TODO: implement
-  Fixedpoint fp = {whole,frac,1};
+  Fixedpoint fp = {whole,frac,1}; //create a Fixedpoint struct, with tag defaulted to 1 (valid and non-negative)
   return fp;
 }
+
+//Returns index of decimal points, or -1 if not found decimal point, -2 if error
 int find_unique_dot_index(const char * str){
-  int count = 0, i = 0, idx = -1;
+  if(!str){
+    return -2;
+  }
+  int count = 0, i = 0, idx = -1; //idx of decimal point defaulted to unfound(-1)
   for (; str[i]; i++){
     if(str[i] == '.'){
-      if(count != 0) return -2; // more than one dot
-      count++;
-      idx = i;
+      if(count != 0) return -2; // if encountered more than one dot, return -2 indicating error
+      count++; 
+      idx = i; //set idx to the index of decimal point
     }
   }
   return idx;
 }
-//return -1 if invalid, 0 if valid and no decimal part, 1 if valid with decimal part
+
+//return -1 if invalid, 0 if valid with no decimal part, 1 if valid with decimal part
 int is_valid_hex_string(const char *hex){ 
+  if(!hex) {
+    return -1;
+  }
   int count = 0, i = 0;
-  if (hex[0] == '-'){//if first character is a minus sign, skip it in the for loop
+  if (hex[0] == '-'){//if first character is a minus sign, skip the sign in the for loop
     i = 1;
   }
   for (; hex[i]; i++){
@@ -43,7 +50,7 @@ int is_valid_hex_string(const char *hex){
     }
     count += (hex[i] == '.'); //counts number of decimal points
   }
-  if(count > 1){
+  if(count > 1){ //if count larger than 1, then invalid
     return -1;
   }
   if (count == 1){
@@ -52,64 +59,58 @@ int is_valid_hex_string(const char *hex){
   return 0;
 }
 Fixedpoint fixedpoint_create_from_hex(const char *hex) {
-  int i, count, padding = 0, is_negative = 0;
+  int padding = 0, is_negative = 0;
   uint64_t whole=0, frac=0;
-  char * token;
   Fixedpoint fp = {whole,frac,0};
-
   if(strlen(hex)==0){
-    return fp;
+    return fp; //if empty string, return. (tag defaulted to 0)
   }
-  int return_code = is_valid_hex_string(hex);
+  int return_code = is_valid_hex_string(hex); //check if valid
   if (return_code == -1){
     fp.tag=0;
-    return fp; //invalid, with tag defaulted to 0
+    return fp; //invalid, with tag set to 0
   }
-
-  if(hex[0] == 45){ // if a minus sign
+  if(hex[0] == 45){ // if starts with a minus sign
     is_negative = 1;
-    fp.tag=-1;
+    fp.tag=-1;  //set tag
   }
   else{
     fp.tag=1;
   }
-
-  int hex_len = strlen(hex);
-  int dot_idx = find_unique_dot_index(hex);
-  int whole_len = dot_idx + 1;
-  int frac_len = hex_len - dot_idx;
+  int hex_len = strlen(hex);  //get string length
+  int dot_idx = find_unique_dot_index(hex);  //get index of decimal point
+  int whole_len = dot_idx + 1;    //set length for whole part's string (+1 for null terminator)
+  int frac_len = hex_len - dot_idx; //set length for frac part's string
   char* whole_str = NULL;
   char* frac_str= NULL;
-
-  if (whole_len != 0){
+  if (whole_len != 0){  //if contains whole part
     whole_str = malloc(whole_len);
     assert(whole_str);
-    strncpy(whole_str,hex,whole_len-1);
+    strncpy(whole_str,hex,whole_len-1); //copy whole parts to the new string
     whole_str[whole_len-1] = 0; //null-terminated
   }
-  if (frac_len != 0){
+  if (frac_len != 0){ //if contains frac part
     frac_str = malloc(frac_len);
     assert(frac_str);
-    strncpy(frac_str,&(hex[dot_idx+1]),frac_len-1);
+    strncpy(frac_str,&(hex[dot_idx+1]),frac_len-1); //copy parts after the decimal point to the new string
     frac_str[frac_len-1]=0;
   }
-
-  if(whole_str && frac_str == NULL){
+  if(whole_str && frac_str == NULL){ //if 
     fp.tag=0;
     return fp;
   }
   if(whole_str){
-    whole = strtoul(whole_str+is_negative,NULL,16); //skip minus sign if negative
+    whole = strtoul(whole_str+is_negative,NULL,16); //add is_negative to parse the parts without minus sign if any
     fp.whole_part=whole;
   }
   if(frac_str){
-    assert(return_code == 1); // make sure there's only one decimal point
+    assert(return_code == 1); // make sure there's at most only one decimal point
     frac=strtoul(frac_str,NULL,16);
     padding = (strlen(frac_str) ) *4;
-    frac = frac << 64-padding;
+    frac = frac << 64-padding; //left shift
     fp.frac_part=frac;
   }
-  free(whole_str);
+  free(whole_str);//deallocate memory
   free(frac_str);
   return fp;
 }
@@ -164,6 +165,7 @@ int fixedpoint_compare(Fixedpoint left, Fixedpoint right) {
 //   1 if val is a valid Fixedpoint value equal to 0;
 //   0 is val is not a valid Fixedpoint value equal to 0
 int fixedpoint_is_zero(Fixedpoint val) {
+   //if zero, it should be valid non-negative with both parts = 0
   int is_valid = (val.tag == 1) && (val.whole_part == 0 && val.frac_part == 0);
   return is_valid;
 }
