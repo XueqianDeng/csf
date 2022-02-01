a9f51ecd96e099bc54b8f2c81577e92a3582f710
chujianyu <yu_chn@outlook.com>
2022-02-01 18:10:25 -0500

    added several tests

--- hw1/csf_assign01/csf_Jan31_HW1/tttttt.c
+++ hw1/csf_assign01/csf_Jan31_HW1/tttttt.c
@@ -6,12 +6,14 @@
 #include <stdint.h>
 #include "fixedpoint.h"
 int main(){
-    Fixedpoint val1 = fixedpoint_create_from_hex("--1!00f2");
-    Fixedpoint val2 = fixedpoint_create_from_hex("f6a5865.00f2");
+    Fixedpoint val1 = fixedpoint_create_from_hex("ab0ec.");
+//   ASSERT(val6.tag==1);
+//   ASSERT(0xab0ecUL == fixedpoint_whole_part(val6));
+  printf("%d\n\n\n",0x0000000000000000UL == fixedpoint_frac_part(val1));
     printf("%lu\n%lu\n%d\n\n",val1.whole_part, val1.frac_part,val1.tag);
-    printf("%lu\n%lu\n%d\n\n",val2.whole_part, val2.frac_part,val2.tag);
+    // printf("%lu\n%lu\n%d\n\n",val2.whole_part, val2.frac_part,val2.tag);
     printf("\n%d\n\n",fixedpoint_is_zero(val1));
-    printf("\n%d\n\n",fixedpoint_is_zero(val2));
+    // printf("\n%d\n\n",fixedpoint_is_zero(val2));
     // char str[]="1";
     // printf("%lu",strtoul(str,NULL,16)<<63);
 }
