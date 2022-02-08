34f66650e085aa45a7cee66ba693c204d8044fa6
chujianyu <yu_chn@outlook.com>
2022-02-08 18:10:55 -0500

    added comments

--- hw1/csf_assign01/csf_Feb6_HW1/fixedpoint.c
+++ hw1/csf_assign01/csf_Feb6_HW1/fixedpoint.c
@@ -129,18 +129,6 @@ uint64_t fixedpoint_frac_part(Fixedpoint val) {
 }
 
 // Compute the sum of two valid Fixedpoint values.
-//
-// Parameters:
-//   left - the left Fixedpoint value
-//   right - the right Fixedpoint value
-//
-// Returns:
-//   if the sum left + right is in the range of values that can be represented
-//   exactly, the sum is returned;
-//   if the sum left + right is not in the range of values that can be
-//   represented, then a value for which either fixedpoint_is_overflow_pos or
-//   fixedpoint_is_overflow_neg returns true is returned (depending on whether
-//   the overflow was positive or negative)
 Fixedpoint fixedpoint_add(Fixedpoint left, Fixedpoint right) {
   Fixedpoint sum = {0,0,left.tag};
   int frac_carry = 0;
@@ -150,33 +138,35 @@ Fixedpoint fixedpoint_add(Fixedpoint left, Fixedpoint right) {
   }
   if (left.tag == right.tag){
     sum.frac_part = left.frac_part + right.frac_part;
-    if(sum.frac_part < left.frac_part || sum.frac_part < right.frac_part) {
+    if(sum.frac_part < left.frac_part || sum.frac_part < right.frac_part) { //determine if carrying is needed
       frac_carry = 1;
       }
     sum.whole_part = left.whole_part + right.whole_part + frac_carry;
-    if(sum.whole_part < left.whole_part || sum.whole_part < right.whole_part) {
-      sum.tag = sum.tag*2; //overflow TODO: maybe it should be only positive overflow
+    if(sum.whole_part < left.whole_part || sum.whole_part < right.whole_part) { //if the sum is smaller than either operands
+      sum.tag = sum.tag*2; //overflow
       return sum;
     }
   }
   else{ //if different signs
     int borrowed = 0, left_is_greater = -1;
-    if(left.whole_part >right.whole_part || (left.whole_part == right.whole_part && left.frac_part>right.frac_part) ) left_is_greater = 1;
-    sum.frac_part = left_is_greater*(left.frac_part - right.frac_part);
+    if(left.whole_part >right.whole_part || (left.whole_part == right.whole_part && left.frac_part>right.frac_part) ){
+      left_is_greater = 1; //find the one if larger absolute value; equal abs.vals will be handled below
+    }
+    sum.frac_part = left_is_greater*(left.frac_part - right.frac_part); //have the larger one minus the smaller one
     if((left_is_greater == 1 && sum.frac_part > left.frac_part) || ((left_is_greater == -1 && sum.frac_part > right.frac_part)) ){
       borrowed = 1;
     }
-    sum.whole_part = left_is_greater*(left.whole_part - right.whole_part) - borrowed;
-    if (sum.frac_part == 0 && sum.whole_part == 0) sum.tag = 1;
-    else sum.tag = left_is_greater==1?left.tag:right.tag;
+    sum.whole_part = left_is_greater*(left.whole_part - right.whole_part) - borrowed; //compute whole part; if borrowing happens, minus one
+    if (sum.frac_part == 0 && sum.whole_part == 0) sum.tag = 1; //handle the case when abs.vals are the same
+    else sum.tag = left_is_greater == 1 ? left.tag:right.tag; //set tag according to the operand that determines over the sign
   }
   return sum;
 }
 
 //perform fixedpoint subtraction 
 Fixedpoint fixedpoint_sub(Fixedpoint left, Fixedpoint right) {
-  Fixedpoint right_negated = {right.whole_part,right.frac_part,-right.tag};
-  return fixedpoint_add(left,right_negated);
+  Fixedpoint right_negated = {right.whole_part,right.frac_part,-right.tag}; //negate the tag of the rhs
+  return fixedpoint_add(left,right_negated);//use add function with negated rhs
 
 }
 
@@ -188,7 +178,7 @@ Fixedpoint fixedpoint_negate(Fixedpoint val) {
 
 //divde fixedpoint by half
 Fixedpoint fixedpoint_halve(Fixedpoint val) {
-  int new_tag = (val.frac_part & 1) == 1 ? (val.tag)*3: val.tag; //if underflow, set tag to 3 or -3
+  int new_tag = (val.frac_part & 1) == 1 ? (val.tag)*3: val.tag; //if underflow, set tag to 3 or -3 to indicate
   Fixedpoint half = {val.whole_part >> 1, (val.frac_part >> 1) + ( (val.whole_part & 1) << 63), new_tag};
   return half;
 }
@@ -265,7 +255,7 @@ int fixedpoint_is_valid(Fixedpoint val) {
   return (abs(val.tag)==1);
 }
 
-//direction = 1 trims leading 0 for whole, =-1 trims trailing 0 for frac                                                        
+//trims leading and trailing 0's (if has fraction part)                                                        
 char* trim_zeros(char* str, int has_frac){ 
   while(str && *str == '0' && *(str+1) && *(str+1)!='.') str++;
   if (has_frac == 1){
@@ -279,19 +269,19 @@ char* trim_zeros(char* str, int has_frac){
 //return the hexidecimal char of the fixedpoint.
 char *fixedpoint_format_as_hex(Fixedpoint val) {
   int has_frac = val.frac_part != 0 ? 1 : 0;
-  int has_minus_sign = val.tag == 1 ? 0 : 1;
-  int len = has_frac ? 32+1 : 16; //32+1 for decimal point
+  int has_minus_sign = val.tag == 1 ? 0 : 1; 
+  int len = has_frac ? 32+1 : 16; //32+1 if has decimal point, otherwise 16
   char *str = (char*) malloc(len+1);
   str[len]  = 0;   //null-terminated
-  sprintf(str,"%016lx",val.whole_part);
+  sprintf(str,"%016lx",val.whole_part); //get hex string
   if(has_frac == 1){
-    str[16]='.';
+    str[16]='.'; //add decimal point if needed
     sprintf(str+17,"%016lx",val.frac_part);
   }
-  char* temp = trim_zeros(str,has_frac);
+  char* temp = trim_zeros(str,has_frac);  //trim all zeros from str
   char* trimmed_str  =(char*) malloc(strlen(temp)+1+has_minus_sign);
-  strcpy(trimmed_str + has_minus_sign,temp);
-  if(has_minus_sign == 1) trimmed_str[0] = '-';
-  free(str);
+  strcpy(trimmed_str + has_minus_sign,temp); //copy the string and make space for the minus sign (if needed)
+  if(has_minus_sign == 1) trimmed_str[0] = '-'; //add minus sign if needed
+  free(str); //free original string
   return trimmed_str;
 }
