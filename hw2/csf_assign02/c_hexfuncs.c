82ab6f32a44d793ce1102a249d2207f2ba797623
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-02-10 18:15:13 -0500

    Update c_hexfuncs.c
    style change

--- hw2/csf_assign02/c_hexfuncs.c
+++ hw2/csf_assign02/c_hexfuncs.c
@@ -5,10 +5,18 @@
 #include "hexfuncs.h"
 
 // TODO: add function implementations here
+/*
+ * 
+ */
 unsigned hex_read(char data_buf[]) {
   //this line uses the ssize_t read (int filedes, void *buffer, size_t size)
   //function in the standard GNU C library. For file descriptor, 0=stdin and 
   // 2=stderr. 
   unsigned actual_number_of_bytes = read(0, *data_buf, 16);
   return actual_number_of_bytes;
+}
+
+// Write given nul-terminated string to standard output.
+void hex_write_string(const char s[]){
+  
 }
