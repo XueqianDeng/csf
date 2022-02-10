1c613973120a7cfd5d2a32a4c9ae6823a3e7f15e
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-02-10 18:09:26 -0500

    Update c_hexfuncs.c
    first implementation of hex_read function

--- hw2/csf_assign02/c_hexfuncs.c
+++ hw2/csf_assign02/c_hexfuncs.c
@@ -5,3 +5,9 @@
 #include "hexfuncs.h"
 
 // TODO: add function implementations here
+unsigned hex_read(char data_buf[]) {
+  //this line uses the ssize_t read (int filedes, void *buffer, size_t size)
+  //function in the standard GNU C library. For file descriptor, 0=stdin and 
+  // 2=stderr. 
+  int actual_number_of_bytes = read (0, *data_buf, 16);
+}
