8dd71fa6b4f8f5fe8ab681a7cc2eb1c30b91926c
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-02-10 18:33:14 -0500

    fix a pointer bug

--- hw2/csf_assign02/c_hexfuncs.c
+++ hw2/csf_assign02/c_hexfuncs.c
@@ -18,7 +18,7 @@ unsigned hex_read(char data_buf[]) {
   //this line uses the ssize_t read (int filedes, void *buffer, size_t size)
   //function in the standard GNU C library. For file descriptor, 0=stdin and 
   // 2=stderr. 
-  unsigned actual_number_of_bytes = read(0, *data_buf, 16);
+  unsigned actual_number_of_bytes = read(0, data_buf, 16);
   return actual_number_of_bytes;
 }
 
