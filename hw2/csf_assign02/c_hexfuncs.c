1a863e1bdbe8c4039d1167735455838b1709e2af
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-02-10 18:13:05 -0500

    Update c_hexfuncs.c
    change to unsigned

--- hw2/csf_assign02/c_hexfuncs.c
+++ hw2/csf_assign02/c_hexfuncs.c
@@ -9,6 +9,6 @@ unsigned hex_read(char data_buf[]) {
   //this line uses the ssize_t read (int filedes, void *buffer, size_t size)
   //function in the standard GNU C library. For file descriptor, 0=stdin and 
   // 2=stderr. 
-  int actual_number_of_bytes = read(0, *data_buf, 16);
+  unsigned actual_number_of_bytes = read(0, *data_buf, 16);
   return actual_number_of_bytes;
 }
