21e946cf8007a60b2ecd779df07309ecda5be6c9
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-02-11 05:02:33 -0500

    main function

--- hw2/csf_assign02/c_hexmain.c
+++ hw2/csf_assign02/c_hexmain.c
@@ -24,9 +24,30 @@ int main(void) {
     // let's ouput the first 8 hex  
     hex_write_string(': ');
     // column and space before the ASCII code for each char
-    for (int i = 0; i < togo; i++) {
-      hex_format_byte_as_hex
+    int position = 0; // there are case that the line is not full.
+    char *databufferhexextractor = databuffer; // hex char counter
+    for (; position < togo; position++) {
+      unsigned char byteval = databufferhexextractor[0];
+      // this shoud take out the character byte
+      stringbuffer = (stringbuffer | 0) & ('\0');
+      // this should reset every in stringbuffer as 0
+      // and a null terminator at the end. 
+      hex_format_byte_as_hex(byteval, stringbuffer);
+      hex_write_string(stringbuffer);
+      hex_write_string(' ');
+      // this would print out the hex char of the character
     }
     // this for loop will write the hex char
+    for(; position < 16; position++) {
+      hex_write_string('   ');
+    }
+    // this is to make up the space if there is no 16 byte to write in the line
+    hex_write_string('  ');
+    // this is the end of writing hex char 
+    for(; *databufferhexextractor; databufferhexextractor++) {
+      hex_write_string(hex_to_printable(databufferhexextractor[0]));
+      //this should output the character if printable
+    }
   }
+  // I believe we are all set with this while loop
 }
