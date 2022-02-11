3a6d18b9204ea83cce50ddf8c97931028f78564a
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-02-11 05:19:35 -0500

    fix a several bugs

--- hw2/csf_assign02/c_hexmain.c
+++ hw2/csf_assign02/c_hexmain.c
@@ -20,9 +20,9 @@ int main(void) {
     // should be 16 bytes except the last line
     hex_format_offset(offsetcounter, stringbuffer);
     // hexidecimal offset is translated into stringbuffer
-    hex_write_string(stringbuffer);
+    hex_write_string((char)stringbuffer);
     // let's ouput the first 8 hex  
-    hex_write_string(': ');
+    hex_write_string((char)': ');
     // column and space before the ASCII code for each char
     int position = 0; // there are case that the line is not full.
     char *databufferhexextractor = databuffer; // hex char counter
@@ -33,22 +33,22 @@ int main(void) {
       // this should reset every in stringbuffer as 0
       // and a null terminator at the end. 
       hex_format_byte_as_hex(byteval, stringbuffer);
-      hex_write_string(stringbuffer);
-      hex_write_string(' ');
+      hex_write_string((char)stringbuffer);
+      hex_write_string((char)' ');
       // this would print out the hex char of the character
     }
     // this for loop will write the hex char
     for(; position < 16; position++) {
-      hex_write_string('   ');
+      hex_write_string((char)'   ');
     }
     // this is to make up the space if there is no 16 byte to write in the line
-    hex_write_string('  ');
+    hex_write_string((char)'  ');
     // this is the end of writing hex char 
     for(; *databufferhexextractor; databufferhexextractor++) {
       hex_write_string(hex_to_printable(databufferhexextractor[0]));
       //this should output the character if printable
     }
-    hex_write_string('\n');
+    hex_write_string((char)'\n');
   }
   // I believe we are all set with this while loop
 }
