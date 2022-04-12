20f50eed3613d81940455097d4de08be7ef45ea1
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-04-11 23:22:17 -0400

    first push everything is fine

--- csf_assign04_cplusplus/magic.cpp
+++ csf_assign04_cplusplus/magic.cpp
@@ -14,5 +14,17 @@
 #include "elf_names.h"
 
 int main(int argc, char **argv) {
-  // TODO: implement
+  if(argc != 2) {
+    std:cerr << "Not Enough Arguments \n";
+    return 1;
+  }
+  int filedescriptor = open(argv[1], O_RDONLY);
+  struct stat statistical_buffer;
+  int reader_come = fstat(filedescriptor, &statistical_buffer);
+  if(reader_come != 0) {
+    std:cerr << "An error has occured\n";
+    return 2;
+  }
+  uint32_t file_size = stat.st_size;
+  void *data
 }
