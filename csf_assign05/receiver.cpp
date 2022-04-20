655474c0406fc77d81f7cdd59996262824e130b5
chujianyu <yu_chn@outlook.com>
2022-04-20 14:53:48 -0400

    cleaned code

--- csf_assign05/receiver.cpp
+++ csf_assign05/receiver.cpp
@@ -7,24 +7,6 @@
 #include "connection.h"
 #include "client_util.h"
 
-// bool concat(char* tag, ){
-//   char filename[128];
-//   char* name = "hello";
-//   char* extension = ".txt";
-
-//   if (sizeof(filename) < strlen(name) + 1 ) { /* +1 is for null character */
-//       fprintf(stderr, "Name '%s' is too long\n", name);
-//       return EXIT_FAILURE;
-//   }
-//   strncpy(filename, name, sizeof(filename));
-
-//   if (sizeof(filename) < (strlen(filename) + strlen(extension) + 1) ) {
-//       fprintf(stderr, "Final size of filename is too long!\n");
-//       return EXIT_FAILURE;
-//   }
-//   strncat(filename, extension, (sizeof(filename) - strlen(filename)) );
-//   printf("Filename is %s\n", filename);
-// }
 int main(int argc, char **argv) {
   if (argc != 5) {
     std::cerr << "Usage: ./receiver [server_address] [port] [username] [room]\n";
@@ -37,35 +19,16 @@ int main(int argc, char **argv) {
   std::string room_name = argv[4];
 
   Connection conn;
-  // std::cout << "before connect" << std::endl;
 
   // TODO: connect to server
-  // int fd = open_clientfd(argv[1], argv[2]);
-  // if (fd < 0) {
-  //   // fatal("Couldn't connect to server"); 
-  // }
   conn.connect(server_hostname, server_port);
   if (!conn.is_open()) {
     std::cerr<< "connection failure" <<std::endl;
     return -1;
   }
-  // std::cout << "connected" << std::endl;
 
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
-  //TODO: send rlogin
-  // std::string usr = "rlogin:";
-  // usr += argv[3];
-  // rio_writen(fd, usr.c_str(), usr.length());
-  // // rio_writen(fd, "\n", 1);
-  // rio_t rio; // read response from server
-  // rio_readinitb(&rio, fd);
-  // char buf[1000];
-  // ssize_t n = rio_readlineb(&rio, buf, sizeof(buf)-1);
-  // if (n > 0) { // print response
-  // buf[n] = '\0'; // add NUL terminator to received message
-  // printf("Received from server: %s", buf);
-  // }
   Message rlogin = {TAG_RLOGIN, username};
   Message ok_response;
   conn.send(rlogin);
@@ -74,20 +37,8 @@ int main(int argc, char **argv) {
     std::cerr << ok_response.data;
     return -1;
   }
-  // std::cout << "[main]login success" << std::endl;
 
   //TODO: send join
-  // std::string join = "join:";
-  // join += argv[4];
-  // rio_writen(fd, join.c_str(), join.length());
-  // // rio_writen(fd, "\n", 1);
-  // rio_readinitb(&rio, fd);
-  // char buf_2[1000];
-  // n = rio_readlineb(&rio, buf_2, sizeof(buf_2)-1);
-  // if (n > 0) { // print response
-  // buf_2[n] = '\0'; // add NUL terminator to received message
-  // printf("Received from server: %s", buf_2);
-  // }
   Message join = {TAG_JOIN, room_name};
   conn.send(join);
   conn.receive(ok_response);
@@ -95,21 +46,11 @@ int main(int argc, char **argv) {
     std::cerr << ok_response.data;
     return -1;
   }
-  // std::cout << "[main]joined successfully" << std::endl;
 
   // TODO: loop waiting for messages from server
   //       (which should be tagged with TAG_DELIVERY)
   Message from_server;
   while (true) {
-    // rio_readinitb(&rio, fd);
-    // char buf_3[1000];
-    // n = rio_readlineb(&rio, buf_3, sizeof(buf_3)-1);
-    // if (n > 0) { // print response
-    //   buf_2[n] = '\0'; // add NUL terminator to received message
-    //   printf("Received from server: %s", buf_2);
-    // } else {
-    //   break;
-    // }
     do {
       conn.receive(from_server);
       if (from_server.tag == TAG_ERR) {
@@ -117,13 +58,8 @@ int main(int argc, char **argv) {
       }
     } while (from_server.tag != TAG_DELIVERY);
 
-  //  std::cout << "received successfully" << std::endl;
    std::vector<std::string> message = from_server.split_payload();
-  //  std::cout<<message.size() <<std::endl;
    std::cout << message[1] <<": " << message[2];
   }
-  
-
-
   return 0;
 }
--- csf_assign05/sender.cpp
+++ csf_assign05/sender.cpp
@@ -7,22 +7,28 @@
 #include "connection.h"
 #include "client_util.h"
 
-
-// void join_rm(int fd, char* rm_num, rio_t rio) {
-//   //TODO: send join
-//     std::string join = "join:";
-//     join += rm_num;
-//     rio_writen(fd, join.c_str(), join.length());
-//     // rio_writen(fd, "\n", 1);
-//     rio_readinitb(&rio, fd);
-//     char buf_2[1000];
-//     int n = rio_readlineb(&rio, buf_2, sizeof(buf_2)-1);
-//     if (n > 0) { // print response
-//       buf_2[n] = '\0'; // add NUL terminator to received message
-//       printf("Received from server: %s", buf_2);
-//     }
-//   }
-
+//return false if entered /quit, true otherwise
+bool send_command(Connection& conn, std::string input) {
+  if (input.compare("/quit") == 0) {
+      Message quit = {TAG_QUIT, ""};
+      conn.send(quit);
+      return false;
+    }
+    else if (input.compare("/leave") == 0) {
+      Message leave = {TAG_LEAVE, ""};
+      conn.send(leave);
+    }
+    else if (input.length() >= 5 && input.substr(0,5).compare("/join") == 0) {
+      std::string rm_name = input.substr(6, input.length());
+      Message join = {TAG_JOIN, rm_name};
+      conn.send(join);
+    }
+    else {
+      Message sendAll = {TAG_SENDALL, input};
+      conn.send(sendAll);
+    }
+    return true;
+}
 
 
 int main(int argc, char **argv) {
@@ -31,13 +37,9 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  std::string server_hostname;
-  int server_port;
-  std::string username;
-
-  server_hostname = argv[1];
-  server_port = std::stoi(argv[2]);
-  username = argv[3];
+  std::string server_hostname = argv[1];
+  int server_port = std::stoi(argv[2]);
+  std::string username = argv[3];
 
   Connection conn;
 
@@ -47,7 +49,6 @@ int main(int argc, char **argv) {
     std::cerr<< "connection failure" <<std::endl;
     return -1;
   }
-  // std::cout << "connected" << std::endl;
 
   // TODO: send slogin message
   Message slogin = {TAG_SLOGIN, username};
@@ -58,84 +59,18 @@ int main(int argc, char **argv) {
     std::cerr << ok_response.data;
     return -1;
   }
-  // std::cout << "login success" << std::endl;
   // TODO: loop reading commands from user, sending messages to
   //       server as appropriate
   std::string input;
-  bool loop = true;
-  while (loop) {
+  bool proceed = true;
+  while (proceed) {
     std::getline(std::cin, input);
     // std::cout << input <<std::endl;
-    if (input.compare("/quit") == 0) {
-      loop = false;
-      Message quit = {TAG_QUIT, ""};
-      conn.send(quit);
-    }
-    else if (input.compare("/leave") == 0) {
-      Message leave = {TAG_LEAVE, ""};
-      conn.send(leave);
-    }
-    else if (input.length() >= 5 && input.substr(0,5).compare("/join") == 0) {
-      std::string rm_name = input.substr(6, input.length());
-      // std::cin >> rm_name;
-      Message join = {TAG_JOIN, rm_name};
-      conn.send(join);
-      // std::cout << "joined successfully to: "<< rm_name << std::endl;
-    }
-    else {
-      Message sendAll = {TAG_SENDALL, input};
-      conn.send(sendAll);
-      // std::cout << "already sent: " << input << std::endl;
-    }
+    proceed = send_command(conn, input);
     conn.receive(ok_response);
     if (ok_response.tag == TAG_ERR) {
     std::cerr << ok_response.data;
     }
   }
-
-  // // // TODO: connect to server
-  // // int fd = open_clientfd(argv[1], argv[2]);
-  // // if (fd < 0) {
-  // //   // fatal("Couldn't connect to server"); 
-  // // }
-
-  // // TODO: send rlogin and join messages (expect a response from
-  // //       the server for each one)
-  // //TODO: send rlogin
-  // std::string usr = "slogin:";
-  // usr += argv[3];
-  // rio_writen(fd, usr.c_str(), usr.length());
-  // // rio_writen(fd, "\n", 1);
-  // rio_t rio; // read response from server
-  // rio_readinitb(&rio, fd);
-  // char buf[1000];
-  // ssize_t n = rio_readlineb(&rio, buf, sizeof(buf)-1);
-  // if (n > 0) { // print response
-  // buf[n] = '\0'; // add NUL terminator to received message
-  // printf("Received from server: %s", buf);
-  // }
-  // bool loop = true;
-  // std::string input;
-  // while (loop) {
-  //   std::cin >> input;
-  //   if (input.compare("/quit") == 0) {
-  //     loop = false;
-  //   }
-  //   else if (input.compare("/leave")) {
-      
-  //   }
-  //   else if (input.compare("/join")) {
-  //     char* rm_num;
-  //     std::cin >> rm_num;
-  //     join_rm(fd,rm_num,rio);
-  //   }
-  // }
-
-  
-
-  
-  
-  
-
   return 0;
 }
