cca03d1d72489e493aa0269cb6ad33096a4370c4
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-04-21 17:25:42 -0400

    Update connection.cpp

--- csf_assign05/connection.cpp
+++ csf_assign05/connection.cpp
@@ -5,12 +5,10 @@
 #include "message.h"
 #include "connection.h"
 
-//TODO: testing
 #include <iostream>
 
 /*
- * To construct a connection object instance with '-1'.
- *
+ * To construct a connection object instance with file descriptor '-1'.
  */
 Connection::Connection()
   : m_fd(-1)
@@ -18,50 +16,69 @@ Connection::Connection()
 }
 
 /*
- * To get the endianness of a valid elf file
+ * To construct a connection object with a given file descriptor. 
  *
  * Parameters:
- *   fd - the pointer to elf header
- *
- * Returns:
- *   a string containning the corresponding endianness of the valid elf file
+ *   fd - file descriptor index
  */
 Connection::Connection(int fd)
   : m_fd(fd)
   , m_last_result(SUCCESS) {
-  // TODO: call rio_readinitb to initialize the rio_t object
   rio_readinitb(&m_fdbuf, m_fd);
 }
 
+/*
+ * To connect the client
+ *
+ * Parameters:
+ *   hostname - the name of the host
+ */
 void Connection::connect(const std::string &hostname, int port) {
-  // TODO: call open_clientfd to connect to the server
+  // call open_clientfd to connect to the server
   m_fd = open_clientfd(hostname.c_str(), std::to_string(port).c_str());
-  // TODO: call rio_readinitb to initialize the rio_t object
+  // call rio_readinitb to initialize the rio_t object
   rio_readinitb(&m_fdbuf, m_fd);
 }
 
+/*
+ * To the destructor function for the connection object.
+ */
 Connection::~Connection() {
   // TODO: close the socket if it is open
-  
 }
 
+/*
+ * To check if the connection is open. 
+ *
+ * Returns:
+ *  the boolean value of whether the connection is open or not. 
+ */
 bool Connection::is_open() const {
   // TODO: return true if the connection is open
   return m_fd >= 0;
 }
 
+/*
+ * This function is used to close the connection object. 
+ */
 void Connection::close() {
-  // TODO: close the connection if it is open
   if (m_fd >= 0) {
     Close(m_fd);
     m_fd = -1;
   }
 }
 
+/*
+ * This function is used to send the message through the connection object. 
+ *
+ * Parameters:
+ *   msg - the string of the message that the user wants to send. 
+ *
+ * Returns:
+ *   whether the message is sent successfully. 
+ */
 bool Connection::send(const Message &msg) {
   // TODO: send a message
-  // return true if successful, false if not
-  // make sure that m_last_result is set appropriately
   std::string tag_payload = msg.tag+":"+msg.data;
   tag_payload += '\n';
   int n = rio_writen(m_fd, tag_payload.c_str(), tag_payload.length());
@@ -72,6 +89,7 @@ bool Connection::send(const Message &msg) {
   m_last_result = Connection::Result::SUCCESS;
   return true;
 }
+
 
 bool Connection::receive(Message &msg) {
   // TODO: send a message, storing its tag and data in msg
