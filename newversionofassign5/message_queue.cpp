2850e2472b606b2cbd59abaac58d6da5c77aa4d5
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-04-30 18:04:30 -0400

    Update message_queue.cpp

--- newversionofassign5/message_queue.cpp
+++ newversionofassign5/message_queue.cpp
@@ -2,19 +2,10 @@
 #include <ctime>
 #include "message_queue.h"
 
-  // pthread_mutex_t m_lock; // must be held while accessing queue
-  // sem_t m_avail;
-  // std::deque<Message *> m_messages;
+
 
 MessageQueue::MessageQueue() {
-  // TODO: initialize the mutex and the semaphore
-//   BoundedQueue *bq = malloc(sizeof(BoundedQueue));
-// bq->data = malloc(max_items * sizeof(void *));
-// bq->max_items = max_items;
-// bq->head = bq->tail = 0;
-// sem_init(&bq->slots, 0, max_items);
-// sem_init(&bq->items, 0, 0);
-// pthread_mutex_init(&bq->lock, NULL);
+  // initialize the mutex and the semaphore
   sem_init(&m_avail, 0, 0);
   pthread_mutex_init(&m_lock, NULL);
 
