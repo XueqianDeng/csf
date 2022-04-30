54f6151b5a5dd68afb8fd974eb9c8c3e96935e6d
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-04-30 18:07:41 -0400

    Update message_queue.cpp

--- newversionofassign5/message_queue.cpp
+++ newversionofassign5/message_queue.cpp
@@ -38,26 +38,24 @@ void MessageQueue::enqueue(Message *msg) {
   sem_post(&m_avail);
 }
 
+/*
+ * To get the message out off the message queue object
+ */
 Message *MessageQueue::dequeue() {
   struct timespec ts;
-
   // get the current time using clock_gettime:
   // we don't check the return value because the only reason
   // this call would fail is if we specify a clock that doesn't
   // exist
   clock_gettime(CLOCK_REALTIME, &ts);
-
   // compute a time one second in the future
   ts.tv_sec += 1;
-
-  // TODO: call sem_timedwait to wait up to 1 second for a message
-  //       to be available, return nullptr if no message is available
+  // call sem_timedwait to wait up to 1 second for a message
+  // to be available, return nullptr if no message is available
   if (sem_timedwait(&m_avail, &ts) == -1) {
     return nullptr;
   }
-  
-
-  // TODO: remove the next message from the queue, return it
+  // remove the next message from the queue, return it
   pthread_mutex_lock(&m_lock);
   if (m_messages.empty()) {
     return nullptr;
