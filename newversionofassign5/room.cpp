489cda3966c3b26cbd104be36aa7f45ec1f7e87a
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-04-30 18:12:03 -0400

    Update room.cpp

--- newversionofassign5/room.cpp
+++ newversionofassign5/room.cpp
@@ -18,38 +18,56 @@ Room::Room(const std::string &room_name)
   : room_name(room_name) {
   // initialize the mutex
   pthread_mutex_init(&lock, NULL);
-
 }
 
+/*
+ * To destruct the room object with the string name of the room
+ */
 Room::~Room() {
-  // TODO: destroy the mutex
+  // destroy the mutex
   pthread_mutex_destroy(&lock);
 }
 
+/*
+ * To add a user memeber object to the room 
+ * 
+ * Parameters:
+ * user - the user to be added
+ */
 void Room::add_member(User *user) {
-  // TODO: add User to the room
+  // add User to the room
   pthread_mutex_lock(&lock);
   members.insert(user);
   pthread_mutex_unlock(&lock);
 }
 
+
+/*
+ * To remove a user memeber object to the room 
+ * 
+ * Parameters:
+ * user - the user to be removed
+ */
 void Room::remove_member(User *user) {
-  // TODO: remove User from the room
+  // remove User from the room
   pthread_mutex_lock(&lock);
   members.erase(user);
   pthread_mutex_unlock(&lock);
 }
 
 
+/*
+ * To broadcast a message in the room 
+ * 
+ * Parameters:
+ * sender_username - the user who is to send the message
+ * message_text - the message to be sent
+ */
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
-  // TODO: send a message to every (receiver) User in the room
+  // send a message to every (receiver) User in the room
   pthread_mutex_lock(&lock);
   for (User* u : members) {
-    // if (u->username.compare(sender_username) == 0) {
-    //   continue;
-    // }
     std::string str = get_room_name() + ":"+sender_username + ":" + message_text;
-
     str.erase( std::remove(str.begin(), str.end(), '\n'), str.end() );
     str += "\n";
     Message *m = new Message(TAG_DELIVERY, str);
