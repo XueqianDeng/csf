619667e6a3330e14a1a8a583c64aabc4971ab46a
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2022-04-30 17:58:02 -0400

    Update server.cpp

--- newversionofassign5/server.cpp
+++ newversionofassign5/server.cpp
@@ -86,74 +86,50 @@ void *worker(void *arg) {
 
   // Just loop reading messages and sending an ok response for each one
   while (true) {
-    // std::cout<<"waiting"<<std::endl;
     if (!info->conn->receive(msg)) {
-      // std::cout<<"[worker] not received "<<std::endl;
       if (info->conn->get_last_result() == Connection::INVALID_MSG) {
-        // std::cout<<"[worker] invalid msg received "<<std::endl;
         info->conn->send(Message(TAG_ERR, "invalid message"));
       }
-      break;
-      
+      break;      
     }
-
-    // if (!info->conn->send(Message(TAG_OK, "this is just a dummy response"))) {
-    //   std::cout<<"[worker] failed to send ok "<<std::endl;
-    //   break;
-    // }
-    // std::cout<<user<<"[worker] server got msg: " << msg.tag <<": " <<msg.data <<std::endl;
+    
     Room* rm;
     if (msg.tag == TAG_JOIN) {
-      // std::cout<<"[worker] trying to add to room "<<std::endl;
-      // pthread_mutex_lock(&info->server->m_lock);//TODO: before this, failed new connections but didn't fail room change
+      //before this, failed new connections but didn't fail room change
       rm = info->server->find_or_create_room(msg.data);
-      // pthread_mutex_unlock(&info->server->m_lock);
-      // std::cout<<"[worker] added to room "<<std::endl;
       if (user != nullptr) { //if receiver
         rm->add_member(user);
       }
       info->conn->send(Message(TAG_OK, "added to room"));
-      // rm->print_member();
     }
 
     if (msg.tag == TAG_SENDALL) {
-      // std::cout<<user<<"[worker] sending... "<<msg.data<<std::endl;
       rm->broadcast_message(username, msg.data);
       info->conn->send(Message(TAG_OK, "sent all to room"));
-      
     }
 
     if (msg.tag == TAG_QUIT) {
-      // std::cout<<user<<"[worker] sending... "<<msg.data<<std::endl;
       info->conn->send(Message(TAG_OK, "quitting"));
       return nullptr;
-      
     }
 
     if (user != nullptr) {
-        // std::cout<<"[worker] fetching deliv... "<<std::endl;
       Message * deque_msg;
       while (true) {
-        
-        deque_msg = user->mqueue.dequeue();
-        
+        deque_msg = user->mqueue.dequeue();     
         if (deque_msg != nullptr) {
-          // std::cout<<"receiver[worker] got: "<<deque_msg->data<<std::endl;
-          // std::cout<<"[worker] receiving delivery"<<std::endl;
           info->conn->send(Message(TAG_DELIVERY, deque_msg->data));
           delete deque_msg;
         }
-        
       }
-
     }
-    
   }
-
   return nullptr;
 }
 
+// namespace
 }
+// namespace
 
 ////////////////////////////////////////////////////////////////////////
 // Server member function implementation
@@ -172,26 +148,18 @@ Server::~Server() {
 bool Server::listen() {
   std::string port = std::to_string(m_port);
   m_ssock = open_listenfd(port.c_str());
-
-  // std::cout<<"[listen]"<<std::endl;
-
   return m_ssock >= 0;
 }
 
 void Server::handle_client_requests() {
   assert(m_ssock >= 0);
-
   while (true) {
     int clientfd = accept(m_ssock, nullptr, nullptr);
     if (clientfd < 0) {
       std::cerr << "Error accepting connection\n";
       return;
     }
-
-    // std::cout<<"[handle] accepted"<<std::endl;
-
     ConnInfo *info = new ConnInfo(new Connection(clientfd), this);
-
     pthread_t thr_id;
     if (pthread_create(&thr_id, nullptr, worker, static_cast<void *>(info)) != 0) {
       std::cerr << "Could not create thread\n";
@@ -206,7 +174,6 @@ Room *Server::find_or_create_room(const std::string &room_name) {
   // data (the map of room names to room objects)
   Guard g(m_lock);
   Room *room;
- 
   auto i = m_rooms.find(room_name);
   if (i == m_rooms.end()) {
     // room does not exist yet, so create it and add it to the map
